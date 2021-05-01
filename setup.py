651c900b730f7ee34bd11498c965e86b4dab32c1
heitor.anjos.nex <heitor.anjos@claro.com.br>
2021-05-01 15:40:39 -0300

    feat: Sponsorlytix Utils Lib creation
    init with sponsorlytix-mongo module

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sponsorlytix_utils',
+    description='Sponsorlytix code utils',
+    author='Sponsorlytix Team',
+    license'MIT',
+)
