e24b99cb8f22ad328717d95e28a28e939141bf72
heitoranjos15 <42981855+heitoranjos15@users.noreply.github.com>
2021-05-01 15:43:45 -0300

    fix: remove license invalid value

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ from setuptools import find_packages, setup
 setup(
     name='sponsorlytix_utils',
     description='Sponsorlytix code utils',
-    author='Sponsorlytix Team',
-    license'MIT',
+    author='Sponsorlytix Team'
 )
