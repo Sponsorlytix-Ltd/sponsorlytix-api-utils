4974ecad1f2bbae046e28761c89508aa6998c9cd
Hugo <fcohugorosa@gmail.com>
2021-06-03 15:25:35 -0300

    bug mongo_client

--- sponsorlytix_mongo/client.py
+++ sponsorlytix_mongo/client.py
@@ -13,7 +13,8 @@ class SponsorlytixMongoClient:
     def get_collection(self, collection_name):
         return self.client[collection_name]
 
-    def get_client(self, db_name='sponsorlytix'):
+    @classmethod
+    def get_client(db_name='sponsorlytix'):
         mongo_url = os.environ.get('MONGO_URL')
         mongo_conn = MongoClient(mongo_url, 27017)
         return mongo_conn[db_name]
