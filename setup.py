4e92b42874cacb957fbccda9290c8c7dea9ec71f
heitoranjos15 <42981855+heitoranjos15@users.noreply.github.com>
2021-07-04 22:38:25 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import find_packages, setup
+from my_pip_package import __version__
 
 setup(
     name='sponsorlytix_utils',
     description='Sponsorlytix code utils',
+    version=__version__,
     author='Sponsorlytix Team'
 )
