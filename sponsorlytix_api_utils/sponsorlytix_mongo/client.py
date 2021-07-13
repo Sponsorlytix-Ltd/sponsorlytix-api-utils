7be1a1a11a1c54069fc8ce6db3700cdf1060ac1e
heitoranjos15 <42981855+heitoranjos15@users.noreply.github.com>
2021-07-12 22:14:39 -0300

    changing mongodb variables

--- sponsorlytix_api_utils/sponsorlytix_mongo/client.py
+++ sponsorlytix_api_utils/sponsorlytix_mongo/client.py
@@ -6,8 +6,8 @@ from pymongo import MongoClient
 class SponsorlytixMongoClient:
 
     def __init__(self, mongodb_config, db_name='sponsorlytix'):
-        url, port = mongodb_config.get('url'), mongodb_config.get('port')
-        client = MongoClient(url, port)
+        host, port = mongodb_config.get('host'), mongodb_config.get('port')
+        client = MongoClient(host, port)
         self.client = client[db_name]
 
     def get_collection(self, collection_name):
