60524503c8899fefe48fcc953f2ffa78228d1a75
Thitiya Chaipanya <47854234+popmeup@users.noreply.github.com>
2022-04-25 23:53:45 +0700

    add drop_database function

--- sponsorlytix_api_utils/sponsorlytix_mongo/client.py
+++ sponsorlytix_api_utils/sponsorlytix_mongo/client.py
@@ -5,12 +5,18 @@ class SponsorlytixMongoClient:
 
     def __init__(self, host, port, db_name):
         client = MongoClient(host, port)
-        self.client = client.get_database(db_name)
+        self.database = client.get_database(db_name)
 
     def get_collection(self, collection_name):
-        return self.client.get_collection(collection_name)
+        return self.database.get_collection(collection_name)
 
     @classmethod
-    def get_client(cls, host, port, db_name):
+    def get_database(cls, host, port, db_name):
         client = MongoClient(host, port)
         return client.get_database(db_name)
+
+    @classmethod
+    def drop_database(self, host, port, db_name):
+        client = MongoClient(host, port)
+        if db_name in client.list_database_names():
+            client.drop_database(db_name)
