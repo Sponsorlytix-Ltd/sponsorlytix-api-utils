f4c982067e65b991627299edb86c8916489421f2
Pop <47854234+popmeup@users.noreply.github.com>
2022-04-26 00:00:05 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [
 setup(
     name='sponsorlytix_api_utils',
     description='Sponsorlytix code utils',
-    version='1.2',
+    version='1.2.1',
     author='Sponsorlytix Team',
     install_requires=requirements,
     setup_requires=['pytest-runner'],
