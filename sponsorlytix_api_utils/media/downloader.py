cc00f05ee637c26a27a7c963e37d70701b8eab8c
swiftstand <officialswiftstand@gmail.com>
2023-03-08 00:42:44 +0100

    added method for youtube downloader

--- sponsorlytix_api_utils/media/downloader.py
+++ sponsorlytix_api_utils/media/downloader.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import shutil
+from pytube import YouTube
 
 
 def download_media_from_url(media_url: str):
@@ -21,3 +22,35 @@ def download_media_from_url(media_url: str):
         return local_path, media_name
     else:
         raise Exception(f'Image {media_name} cannot be downloaded.')
+
+
+
+def download_youtube_video_from_url(video_url: str):
+
+    file_name = video_url.split("/")[-1].split("?")[-1] +  '.mp4'
+
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    current_directory = os.path.join(current_directory, 'youtube_downloads')
+    if not os.path.exists(current_directory):
+        os.makedirs(current_directory)
+
+    try:
+        # object creation using YouTube
+        # which was imported in the beginning
+        yt = YouTube(video_url)
+
+    except:
+        print("Connection Error") #to handle exception
+
+    # filters out all the files with "mp4" extension
+    mp4files = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+
+    try:
+        # downloading the video
+        file_path = mp4files.download(output_path=current_directory, filename= file_name, )
+    except:
+        print("Error!")
+    print('Task Completed!')
+
+    return file_path, file_name
+
